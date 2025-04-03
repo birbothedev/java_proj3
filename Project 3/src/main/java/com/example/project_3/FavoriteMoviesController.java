e7ce9eec458a585bba91cc8df183f1f86b49ed9f
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-04-03 12:02:28 -0400

    write to file, temp list, other functionality working

--- Project 3/src/main/java/com/example/project_3/FavoriteMoviesController.java
+++ Project 3/src/main/java/com/example/project_3/FavoriteMoviesController.java
@@ -1,23 +1,22 @@
 package com.example.project_3;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-public class FavoriteMoviesController {
-    @FXML
-    private Label welcomeText;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ResourceBundle;
 
-    @FXML
-    private Label titleText;
+public class FavoriteMoviesController implements Initializable{
 
     @FXML
     private TextField movieNameField;
 
     @FXML
-    private ChoiceBox genreBox;
-
-    @FXML
-    private Button addMovieButton;
+    private ChoiceBox<String> genreBox;
 
     @FXML
     private CheckBox actionCheckbox;
@@ -35,7 +34,10 @@ public class FavoriteMoviesController {
     private CheckBox fantasyCheckbox;
 
     @FXML
-    private ListView moviesListView;
+    private ListView<String> moviesListView;
+
+    @FXML
+    private ListView<String> tempMovieListView;
 
     @FXML
     private Button saveMoviesListButton;
@@ -44,7 +46,55 @@ public class FavoriteMoviesController {
     private Button loadMoviesListButton;
 
     @FXML
-    protected void onAddMovieButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button deleteMovie;
+
+    private final ArrayList<Movie> movieArrayList = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        genreBox.getItems().addAll("romance", "thriller", "comedy", "action", "fantasy/sci-fi");
     }
+
+    @FXML
+    protected void onAddMovieButtonClick() {
+        String movieName = movieNameField.getText();
+        String movieGenre = genreBox.getSelectionModel().getSelectedItem();
+        Movie movie = new Movie(movieName, movieGenre);
+
+        movieArrayList.add(new Movie(movieName, movieGenre));
+        tempMovieListView.getItems().add(movie + "\n");
+
+        movieNameField.clear();
+    }
+
+    @FXML
+    protected void onSaveMoviesListButtonClick(){
+        // create file
+        File file = new File("movies.txt");
+
+        // write to file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+            for (Movie movie : movieArrayList){
+                writer.write(movie.toString());
+                writer.newLine();
+            }
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        tempMovieListView.getItems().clear();
+    }
+
+    @FXML
+    protected void onDeleteMovieButtonClick(){
+    }
+
+    @FXML
+    protected void onLoadMoviesListButtonClick(){
+
+    }
+
 }
--- Project 3/src/main/java/com/example/project_3/Movie.java
+++ Project 3/src/main/java/com/example/project_3/Movie.java
@@ -24,4 +24,9 @@ public class Movie {
     public void setGenre(String genre) {
         this.genre = genre;
     }
+
+    @Override
+    public String toString(){
+        return name + ", " + genre;
+    }
 }
